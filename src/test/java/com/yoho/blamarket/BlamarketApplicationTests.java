a2a9cf2c4152357c14b5cbf386921d9015fa29f4
HeoYou <lov21y_@naver.com>
2022-08-15 23:23:54 +0900

    first commit

---
+++ src/test/java/com/yoho/blamarket/BlamarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoho.blamarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlamarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
