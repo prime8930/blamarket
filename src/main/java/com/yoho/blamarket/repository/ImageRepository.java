aca54f7e804a01352746592103f1e4ccdc7906ba
prime8930 <prime8930@gmail.com>
2022-10-06 03:07:03 +0900

    게시판 수정 및 댓글 API 추가

--- src/main/java/com/yoho/blamarket/repository/ImageRepository.java
+++ src/main/java/com/yoho/blamarket/repository/ImageRepository.java
@@ -1,8 +1,8 @@
 package com.yoho.blamarket.repository;
 
-import com.yoho.blamarket.dto.board.RequestAllPostsDto;
 import com.yoho.blamarket.entity.ImageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -10,4 +10,6 @@ public interface ImageRepository extends JpaRepository<ImageEntity, Long> {
     /** JpaRepository<T, ID> 타입 명시할 때는 Entity 타입, PK 타입을 명시한다. */
     List<ImageEntity> findByItemId(long itemId);
 
+    @Transactional
+    int deleteByItemId(long itemId);
 }
